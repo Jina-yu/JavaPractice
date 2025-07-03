32ba6b6fc043a7c9ed50e72f88b93afbf101e3f6
Jina Camellia Yoo <kamelie1004@gmail.com>
2025-07-03 02:54:57 -0700

    07/03 JAVA Practice

---
+++ java/java-mid1/src/lang/wrapper/MyIntegerNullMain1.java
@@ -0,0 +1,21 @@
+package lang.wrapper;
+
+public class MyIntegerNullMain1 {
+    public static void main(String[] args) {
+        MyInteger[] intArr = {new MyInteger(-1), new MyInteger(0), new MyInteger(1)};
+        System.out.println(findValue(intArr, -1));
+        System.out.println(findValue(intArr, 0));
+        System.out.println(findValue(intArr, 1));
+        System.out.println(findValue(intArr, 100));
+    }
+
+    private static MyInteger findValue(MyInteger[] intArr, int target) {
+        for (MyInteger myInteger : intArr) {
+            if (myInteger.getValue() == target) {
+                return myInteger;
+            }
+        }
+        return null; //뭐라도 반환해야함. int는 항상 값이 있으니까.
+    }
+
+}
