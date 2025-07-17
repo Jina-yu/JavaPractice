d55d3541c8dcee1f3ec33acadef9808a87b66188
Jina Camellia Yoo <kamelie1004@gmail.com>
2025-07-16 22:15:01 -0700

    07/17 JAVA Practice

---
+++ java/java-mid1/src/enumeration/ref2/DiscountService.java
@@ -0,0 +1,11 @@
+package enumeration.ref2;
+
+
+
+public class DiscountService {
+    public int discount(Grade grade, int price) {
+
+        //10000 * (20 / 100) -> 2000원
+        return price * grade.getDiscountPercent() / 100;
+    }
+}
